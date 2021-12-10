bc3e569b34dc66b86eb457401ba58e531e413c27
Vincent <vincentgauthier068@gmail.com>
2021-12-10 10:15:53 -0800

    last-index-of

---
+++ last-index.js
@@ -0,0 +1,19 @@
+const lastIndexOf = (array, number) => {
+  let mySet = new Set(array);
+  if (mySet.has(number)) {
+    for (let i = array.length - 1; i >= 0; i--) {
+      if (number === array[i]) {
+        return i;
+      }
+    }
+  } else {
+    return -1;
+  }
+};
+// console.log(lastIndexOf([0, 1, 4, 1, 2], 1), "=?", 3);
+// console.log(lastIndexOf([0, 1, 4, 1, 2], 2), "=?", 4);
+// console.log(lastIndexOf([0, 1, 4, 1, 2], 3), "=?", -1);
+// console.log(lastIndexOf([5, 5, 5], 5), "=?", 2);
+// console.log(lastIndexOf([], 3), "=?", -1);
+
+console.log(lastIndexOf([5, 5, 5, 5, 0], 5));
